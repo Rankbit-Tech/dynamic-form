69e5a1523b227b14c0d5859c5b017dd7b91d378f
Pratik Soni <pratik.soni@simformsolutions.com>
2024-06-06 17:16:16 +0530

    Add layout

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,14 @@ function App() {
   return (
     <FormProvider>
       <DndProvider backend={HTML5Backend}>
-
+        <div className='w-full h-screen flex'>
+          <div className='h-full  w-full flex-1'>
+            <div className='w-full bg-white h-10'>
+              <h1>Dropable area</h1>
+            </div>
+          </div>
+          <div className='min-w-[350px] border-l-[1px] border-gray-300 bg-white p-5'>Dragable area</div>
+        </div>
       </DndProvider>
     </FormProvider>
   );
